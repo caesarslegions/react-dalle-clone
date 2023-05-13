a4d20cdb2e56749da3b6b9da4f257d3162c31699
Jonah Popp <jonah.popp@gmail.com>
2023-05-13 11:23:34 -0500

    added API KEY in env and added functionality to the "generate" button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import {useState} from "react"
+
+
 
 const App = () => {
+  const [images, setImages] = useState(null)
+  const [value, setValue] = useState(null)
 
 const surpriseOptions = [
   'A blue ostrich eating melon',
   'A matisse style shark on the telephone',
   'A pineapple sunbathing',
 ]
+
+const getImages = async() => {
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "BLUGH"
+      }),
+      headers: {
+        "Content-type": "application/json"
+      }
+    }
+    const response = await fetch('http://localhost:8000/images', options)
+    const data = await response.json()
+    console.log(data)
+    setImages(data)
+  } catch (error) {
+    console.error(error)
+  }
+}
 
   return (
     <div className="app">
@@ -14,10 +39,18 @@ const surpriseOptions = [
           <span className="surprise">Surprise me</span>
         </p>
         <div className="input-container">
-          <input placeholder="a purple banan floating in space..."/>
-          <button>Generate</button>
+          <input
+            value={value}
+            placeholder="a purple banan floating in space..."
+            onChange={e => setValue(e.target.value)}
+          />
+          <button onClick={getImages}>Generate</button>
         </div>
-      <section className="image-section"></section>
+      <section className="image-section">
+        {images?.map((image, _index) => (
+          <img key={_index} src={image.url} alt={`Generated image of ${value}`}/>
+        ))}
+      </section>
 
     </div>
   )
