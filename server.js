a1b655841c0ca006837b870da62bd79b759f7177
Jonah Popp <jonah.popp@gmail.com>
2023-05-13 14:00:24 -0500

    .env added to gitignore

--- server.js
+++ server.js
@@ -15,15 +15,14 @@ const openai = new OpenAIApi(configuration)
 app.post('/images', async (req, res) => {
     try {
         const response = await openai.createImage({
-            prompt: "A vibrant purple banana",
-            n: 2,
+            prompt: "An elf from Dungeons and Dragons holding a vibrant blue banana",
+            n: 4,
             size: "1024x1024",
         })
         console.log(response.data.data)
         res.send(response.data.data)
     } catch (error) {
         console.error(error)
-
     }
 
 
